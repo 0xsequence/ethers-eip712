4bf0fa98916e6513a8623fad5bede7c48d530614
Peter Kieltyka <peter.kieltyka@gmail.com>
2020-08-09 11:36:33 -0400

    more tests for complex types

--- src/typed-data.ts
+++ src/typed-data.ts
@@ -21,14 +21,13 @@ export interface TypedDataDomain {
   version?: string
   chainId?: number
   verifyingContract?: string
+  salt?: string
 }
 
 export const TypedDataUtils = {
   encodeDigest(typedData: TypedData): Uint8Array {
     const eip191Header = ethers.utils.arrayify('0x1901')
-
     const domainHash = TypedDataUtils.hashStruct(typedData, 'EIP712Domain', typedData.domain)
-    
     const messageHash = TypedDataUtils.hashStruct(typedData, typedData.primaryType, typedData.message)
 
     const pack = ethers.utils.solidityPack(
@@ -41,7 +40,8 @@ export const TypedDataUtils = {
   },
 
   encodeData(typedData: TypedData, primaryType: string, data: object): Uint8Array {
-    const args = typedData.types[primaryType]
+    const types = typedData.types
+    const args = types[primaryType]
     if (!args || args.length === 0) {
       throw new Error(`TypedDataUtils: ${typedData.primaryType} type is not unknown`)
     }
@@ -50,54 +50,63 @@ export const TypedDataUtils = {
     const abiTypes: string[] = []
     const abiValues: any[] = []
 
-    for (let i=0; i < args.length; i++) {
-      const arg = args[i]
-      const dataValue = data[arg.name]
+    const typeHash = TypedDataUtils.typeHash(typedData.types, primaryType)
+    abiTypes.push('bytes32')
+    abiValues.push(ethers.utils.zeroPad(typeHash, 32))
 
-      if (!dataValue || dataValue === null || dataValue === undefined || dataValue === '') {
-        throw new Error(`data value missing for type ${primaryType} with argument name ${arg.name}`)
+    const encodeField = (name: string, type: string, value: any): (string | Uint8Array)[] => {
+      if (types[type] !== undefined) {
+        return ['bytes32', ethers.utils.arrayify(
+          ethers.utils.keccak256(TypedDataUtils.encodeData(typedData, type, value))
+        )]
       }
 
-      if (arg.type === 'bytes' || arg.type === 'string') {
-        abiTypes.push('bytes32')
-
+      if (type === 'bytes' || type === 'string') {
         let v: any
-        if (arg.type === 'string') {
-          v = ethers.utils.toUtf8Bytes(dataValue)
+        if (type === 'string') {
+          v = ethers.utils.toUtf8Bytes(value)
         } else {
-          v = ethers.utils.arrayify(dataValue)
+          v = ethers.utils.arrayify(value)
         }
+        return ['bytes32', ethers.utils.arrayify(
+          ethers.utils.hexZeroPad(ethers.utils.keccak256(v), 32)
+        )]
 
-        abiValues.push(
-          ethers.utils.arrayify(
-            ethers.utils.hexZeroPad(ethers.utils.keccak256(v), 32)
+      } else if (type.lastIndexOf('[') > 0) {
+        const t = type.slice(0, type.lastIndexOf('['))
+        const v = value.map((item) => encodeField(name, t, item))
+        return ['bytes32', ethers.utils.arrayify(
+            ethers.utils.keccak256(
+              ethers.utils.arrayify(
+                abiCoder.encode(
+                  v.map(([tt]) => tt),
+                  v.map(([, vv]) => vv)
+                )
+              )
+            )
           )
-        )
+        ]
+
       } else {
-        abiTypes.push(arg.type)
-        abiValues.push(dataValue)
+        return [type, value]
       }
     }
 
-    if (args.length !== abiTypes.length || abiTypes.length !== abiValues.length) {
-      throw new Error('argument coding failed to encode all values')
+    for (const field of args) {
+      const [type, value] = encodeField(field.name, field.type, data[field.name]);
+      abiTypes.push(type as string)
+      abiValues.push(value)
     }
 
     return ethers.utils.arrayify(abiCoder.encode(abiTypes, abiValues))
   },
 
   hashStruct(typedData: TypedData, primaryType: string, data: object): Uint8Array {
-    const typeHash = TypedDataUtils.typeHash(typedData.types, primaryType)
-
-    const encodedData = TypedDataUtils.encodeData(typedData, primaryType, data)
-
-    const pack = ethers.utils.solidityPack(
-      ['bytes32', 'bytes'],
-      [ethers.utils.zeroPad(typeHash, 32), encodedData]
+    return ethers.utils.arrayify(
+      ethers.utils.keccak256(
+        TypedDataUtils.encodeData(typedData, primaryType, data)
+      )
     )
-
-    const hashPack = ethers.utils.keccak256(pack)
-    return ethers.utils.arrayify(hashPack)
   },
 
   typeHash(typedDataTypes: TypedDataTypes, primaryType: string): Uint8Array {
@@ -121,16 +130,18 @@ export const TypedDataUtils = {
 
     for (let i=0; i < args.length; i++) {
       const arg = args[i]
+      const arrayArg = arg.type.indexOf('[')
+      const argType = arrayArg < 0 ? arg.type : arg.type.slice(0, arrayArg)
 
-      if (typedDataTypes[arg.type] && typedDataTypes[arg.type].length > 0) {
+      if (typedDataTypes[argType] && typedDataTypes[argType].length > 0) {
         let set = false
         for (let x=0; x < subTypes.length; x++) {
-          if (subTypes[x] === arg.type) {
+          if (subTypes[x] === argType) {
             set = true
           }
         }
         if (!set) {
-          subTypes.push(arg.type)
+          subTypes.push(argType)
         }
       }
       
@@ -148,9 +159,41 @@ export const TypedDataUtils = {
     }
 
     return s
+  },
+
+  domainType(domain: TypedDataDomain): TypedDataArgument[] {
+    const type: TypedDataArgument[] = []
+    if (domain.name) {
+      type.push({ name: 'name', type: 'string' })
+    }
+    if (domain.version) {
+      type.push({ name: 'version', type: 'string' })
+    }
+    if (domain.chainId) {
+      type.push({ name: 'chainId', type: 'uint256' })
+    }
+    if (domain.verifyingContract) {
+      type.push({ name: 'verifyingContract', type: 'address' })
+    }
+    if (domain.salt) {
+      type.push({ name: 'salt', type: 'bytes32' })
+    }
+    return type
   }
+
+  // buildTypedData(): TypedData {
+  //   return {}
+  // }
 }
 
 export const encodeTypedDataDigest = (typedData: TypedData): Uint8Array => {
   return TypedDataUtils.encodeDigest(typedData)
+}
+
+// export const buildTypedData = (): TypedData => {
+//   return TypedDataUtils.buildTypedData()
+// }
+
+export const domainType = (domain: TypedDataDomain): TypedDataArgument[] => {
+  return TypedDataUtils.domainType(domain)
 }
--- tests/typed-data.test.ts
+++ tests/typed-data.test.ts
@@ -105,4 +105,184 @@ describe('TypedData', () => {
     expect(digestHex).toEqual('0x2218fda59750be7bb9e5dfb2b49e4ec000dc2542862c5826f1fe980d6d727e95')
   })
 
+  test('encoding-3', () => {
+    const typedData = {
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
+        ],
+        Person: [
+          { name: 'name', type: 'string' },
+          { name: 'wallets', type: 'address[]' },
+        ],
+        Mail: [
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person[]' },
+          { name: 'contents', type: 'string' },
+        ],
+        Group: [
+          { name: 'name', type: 'string' },
+          { name: 'members', type: 'Person[]' },
+        ],
+      },
+      domain: {
+        name: 'Ether Mail',
+        version: '1',
+        chainId: 1,
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+      },
+      primaryType: 'Mail' as const,
+      message: {
+        from: {
+          name: 'Cow',
+          wallets: [
+            '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+            '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+          ],
+        },
+        to: [{
+          name: 'Bob',
+          wallets: [
+            '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+            '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+            '0xB0B0b0b0b0b0B000000000000000000000000000',
+          ],
+        }],
+        contents: 'Hello, Bob!',
+      },
+    }
+
+    expect(
+      TypedDataUtils.encodeType(typedData.types, 'Group')
+    ).toEqual('Group(string name,Person[] members)Person(string name,address[] wallets)')
+
+    expect(
+      TypedDataUtils.encodeType(typedData.types, 'Person')
+    ).toEqual('Person(string name,address[] wallets)')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.typeHash(typedData.types, 'Person')
+      )
+    ).toEqual('0xfabfe1ed996349fc6027709802be19d047da1aa5d6894ff5f6486d92db2e6860')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.encodeData(typedData, 'Person', typedData.message.from)
+      )
+    ).toEqual(
+      `0x${[
+        'fabfe1ed996349fc6027709802be19d047da1aa5d6894ff5f6486d92db2e6860',
+        '8c1d2bd5348394761719da11ec67eedae9502d137e8940fee8ecd6f641ee1648',
+        '8a8bfe642b9fc19c25ada5dadfd37487461dc81dd4b0778f262c163ed81b5e2a',
+      ].join('')}`
+    )
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.hashStruct(typedData, 'Person', typedData.message.from)
+      )
+    ).toEqual('0x9b4846dd48b866f0ac54d61b9b21a9e746f921cefa4ee94c4c0a1c49c774f67f')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.encodeData(typedData, 'Person', typedData.message.to[0])
+      )
+    ).toEqual(
+      `0x${[
+        'fabfe1ed996349fc6027709802be19d047da1aa5d6894ff5f6486d92db2e6860',
+        '28cac318a86c8a0a6a9156c2dba2c8c2363677ba0514ef616592d81557e679b6',
+        'd2734f4c86cc3bd9cabf04c3097589d3165d95e4648fc72d943ed161f651ec6d',
+      ].join('')}`
+    )
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.hashStruct(typedData, 'Person', typedData.message.to[0])
+      )
+    ).toEqual('0xefa62530c7ae3a290f8a13a5fc20450bdb3a6af19d9d9d2542b5a94e631a9168')
+
+    expect(
+      TypedDataUtils.encodeType(typedData.types, 'Mail')
+    ).toEqual('Mail(Person from,Person[] to,string contents)Person(string name,address[] wallets)')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.typeHash(typedData.types, 'Mail')
+      )
+    ).toEqual('0x4bd8a9a2b93427bb184aca81e24beb30ffa3c747e2a33d4225ec08bf12e2e753')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.encodeData(typedData, typedData.primaryType, typedData.message)
+      )
+    ).toEqual(
+      `0x${[
+        '4bd8a9a2b93427bb184aca81e24beb30ffa3c747e2a33d4225ec08bf12e2e753',
+        '9b4846dd48b866f0ac54d61b9b21a9e746f921cefa4ee94c4c0a1c49c774f67f',
+        'ca322beec85be24e374d18d582a6f2997f75c54e7993ab5bc07404ce176ca7cd',
+        'b5aadf3154a261abdd9086fc627b61efca26ae5702701d05cd2305f7c52a2fc8',
+      ].join('')}`
+    )
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.hashStruct(typedData, typedData.primaryType, typedData.message)
+      )
+    ).toEqual('0xeb4221181ff3f1a83ea7313993ca9218496e424604ba9492bb4052c03d5c3df8')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.hashStruct(typedData, 'EIP712Domain', typedData.domain)
+      )
+    ).toEqual('0xf2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090f')
+
+    expect(
+      ethers.utils.hexlify(
+        TypedDataUtils.encodeDigest(typedData)
+      )
+    ).toEqual('0xa85c2e2b118698e88db68a8105b794a8cc7cec074e89ef991cb4f5f533819cc2')
+  })
+
+  test('encoding-4', () => {
+    const typedData = {
+      types: {
+        EIP712Domain: [
+          {name: "name", type: "string"},
+          {name: "version", type: "string"},
+          {name: "chainId", type: "uint256"},
+          {name: "verifyingContract", type: "address"},
+        ],
+        Person: [
+          {name: "name", type: "string"},
+          {name: "wallet", type: "address"},
+          {name: 'count', type: 'bytes8'}
+        ]
+      },
+      primaryType: 'Person' as const,
+      domain: {
+        name: 'Ether Mail',
+        version: '1',
+        chainId: 1,
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
+      },
+      message: {
+        'name': 'Bob',
+        'wallet': '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        'count': '0x1122334455667788'
+      }
+    }
+
+    const domainHash = TypedDataUtils.hashStruct(typedData, 'EIP712Domain', typedData.domain)
+    const domainHashHex = ethers.utils.hexlify(domainHash)
+    expect(domainHashHex).toEqual('0xf2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090f')
+
+    const digest = TypedDataUtils.encodeDigest(typedData)
+    const digestHex = ethers.utils.hexlify(digest)
+    expect(digestHex).toEqual('0x2a3e64893ed4ba30ea34dbff3b0aa08c7677876cfdf7112362eccf3111f58d1d')
+  })
+  
 })
